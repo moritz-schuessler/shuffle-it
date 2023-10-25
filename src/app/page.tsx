4b903844d3663796f39f64322bfba09d23486570
Mosch <mail@moritzschuessler.de>
2023-10-25 22:56:48 +0200

    Added a Signout Button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,12 @@
+import {SignOutButton} from "@/components/auth/buttons";
+
 const Home = () => {
     return (
         <main className="">
+            <SignOutButton />
         </main>
     )
 }
+
 
 export default Home
--- src/components/auth/buttons.tsx
+++ src/components/auth/buttons.tsx
@@ -1,11 +1,14 @@
 'use client'
 
-import {ClientSafeProvider, signIn} from 'next-auth/react';
+import {signIn, signOut, ClientSafeProvider} from 'next-auth/react';
 
-const SignInButton = ({provider}: {provider: ClientSafeProvider}) => {
+const SignInButton = ({ provider }: { provider:  ClientSafeProvider }) => {
     return <button onClick={() => signIn(provider.id)}>Sign in with {provider.name}</button>
 
 }
 
+const SignOutButton = () => {
+    return <button onClick={() => signOut()}>Sign out</button>;
+}
 
-export {SignInButton}
+export { SignInButton, SignOutButton }
