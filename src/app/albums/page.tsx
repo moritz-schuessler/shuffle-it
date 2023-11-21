221b0d83c8f41233aa386cd0156e4282368f78be
Mosch <mail@moritzschuessler.de>
2023-11-21 21:55:03 +0100

    Added functionality to load more albums on scroll with react-query and react-intersection-observer | using grid instead of flex

--- src/app/albums/page.tsx
+++ src/app/albums/page.tsx
@@ -1,35 +1,63 @@
 'use client'
 
+import { useEffect, useRef} from "react";
+import { useSession } from "next-auth/react";
+import { useInView } from 'react-intersection-observer'
+import { useInfiniteQuery } from "@tanstack/react-query";
+
 import Album from "@/components/Album";
 import { getAlbums } from "@/lib/spotifyApi";
-import { useQuery } from "@tanstack/react-query";
-import { useSession } from "next-auth/react";
 
 const Albums = () => {
     const { data: session } = useSession()
 
-    const { data, status} = useQuery({ queryKey: ['albums', session?.access_token], queryFn: getAlbums, enabled: !!session?.access_token})
+    const rootRef= useRef(null)
+    const { ref, inView } = useInView({
+        threshold: 0,
+        rootMargin: "100%",
+        root: rootRef.current,
+        triggerOnce: false
+    })
+
+    const { data, status, fetchNextPage, hasNextPage, isFetchingNextPage } = useInfiniteQuery({
+        queryKey: ['albums', session?.access_token],
+        queryFn: getAlbums,
+        enabled: !!session?.access_token,
+        initialPageParam: 'https://api.spotify.com/v1/me/albums?offset=0&limit=22&locale=*',
+        getNextPageParam: (lastPage) => lastPage.next
+    })
+
+    useEffect(() => {
+        if (inView) {
+            fetchNextPage()
+        }
+    }, [fetchNextPage, inView])
 
     if (status !== 'success') {
         return 'Loading...'
     }
 
-    const {items: albums} = data!
+    const albums = data?.pages.flatMap((page) => page.items)
 
     return (
-        <main>
-            <h2>
-                Albums
-            </h2>
-            <div className='flex flex-row flex-wrap justify-between gap-[2rem] h-full'>
-                {
-                    albums.map(({album}) => {
-                        return <Album key={album.id} album={album} access_token={session?.access_token!}/>
-                    })
-                }
-            </div>
+        <main className='grid col-start-auto grid-cols-5 grid-auto gap-[2rem] h-full' ref={rootRef.current}>
+            {
+                albums.map((album, i) => {
+                    if (albums?.length === i + 1  && hasNextPage) {
+                        return (
+                            <div key={album.album.id} className='flex' ref={ref}>
+                                <Album album={album.album} access_token={session!.access_token!} />
+                            </div>
+                            )
+                    }
+                    return <Album key={album.album.id} album={album.album} access_token={session!.access_token!} />
+                })
+            }
+            {
+                hasNextPage || isFetchingNextPage?<div>Loading...</div>:null
+            }
         </main>
-    )
+        )
 }
 
 export default Albums
--- src/lib/spotifyApi.ts
+++ src/lib/spotifyApi.ts
@@ -1,14 +1,14 @@
 import {QueryKey} from "@tanstack/query-core";
 
-const getAlbums = async ({ queryKey }:{queryKey:QueryKey}) => {
+const getAlbums = async ({ queryKey, pageParam  }:{queryKey:QueryKey, pageParam: string}) => {
     const [_key, access_token ] = queryKey
-    
-        const data = await fetch('https://api.spotify.com/v1/me/albums?offset=0&limit=20&locale=*', {
-            method: 'GET',
-            headers: {
-                Authorization: `Bearer ${access_token}`
-            },
-        })
+
+    const data = await fetch(pageParam, {
+        method: 'GET',
+        headers: {
+            Authorization: `Bearer ${access_token}`
+        },
+    })
     return await data.json() as Albums
 }
 
