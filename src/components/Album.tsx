e689877cf3dac8082e219d09a76995f86889dfc8
Mosch <mail@moritzschuessler.de>
2023-11-30 23:55:45 +0100

    Used prettier

--- src/components/Album.tsx
+++ src/components/Album.tsx
@@ -1,47 +1,51 @@
-import Image from "next/image";
-import { useMutation } from "@tanstack/react-query";
-import { putPlayback } from "@/lib/spotifyApi";
+import Image from 'next/image';
+import { useMutation } from '@tanstack/react-query';
+import { putPlayback } from '@/lib/spotifyApi';
 
 interface Props {
-    album: Album
-    access_token: string
+  album: Album;
+  access_token: string;
 }
 
-const Album = ({album, access_token}:Props) => {
-    const mutation = useMutation({
-        mutationFn: ({access_token, uri}:{access_token:string, uri:string}) => {
-            return putPlayback(access_token, uri)
-        }
-    })
-    
-    const {name, uri, artists, images} = album
-    
-    return (
-        <button onClick={() => mutation.mutate({ access_token, uri})} className='flex flex-col grow gap-[1rem] p-[1rem] bg-[#0a0a0a]'>
-            <div className='flex grow'>
-                <Image
-                    src={images[0].url}
-                    alt={`Album Cover of ${name}`}
-                    width={300}
-                    height={300}
-                    unoptimized
-                    className='grow aspect-square'
-                />   
-            </div>
-            <div className='flex flex-col gap-[.5rem]'>
-                <a>
-                    {name}
-                </a>
-                <div>
-                    {artists.map((artist) => artist.name).join(', ')}
-                </div>
-            </div>
-        </button>
+const Album = ({ album, access_token }: Props) => {
+  const mutation = useMutation({
+    mutationFn: ({
+      access_token,
+      uri,
+    }: {
+      access_token: string;
+      uri: string;
+    }) => {
+      return putPlayback(access_token, uri);
+    },
+  });
 
-        )
-}
+  const { name, uri, artists, images } = album;
 
-export default Album
+  return (
+    <button
+      onClick={() => mutation.mutate({ access_token, uri })}
+      className='flex flex-col grow gap-[1rem] p-[1rem] bg-[#0a0a0a]'
+    >
+      <div className='flex grow'>
+        <Image
+          src={images[0].url}
+          alt={`Album Cover of ${name}`}
+          width={300}
+          height={300}
+          unoptimized
+          className='grow aspect-square'
+        />
+      </div>
+      <div className='flex flex-col gap-[.5rem]'>
+        <a>{name}</a>
+        <div>{artists.map((artist) => artist.name).join(', ')}</div>
+      </div>
+    </button>
+  );
+};
+
+export default Album;
 
 /*
         <div className='flex flex-col grow gap-[.5rem] p-[1rem] bg-white bg-opacity-10'>
--- src/types/spotifyApi/album.d.ts
+++ src/types/spotifyApi/album.d.ts
@@ -1,107 +1,107 @@
 interface Albums {
-    "href": string,
-    "limit": number,
-    "next": string,
-    "offset": number,
-    "previous": string,
-    "total": number,
-    items: AlbumContainer[]
+  href: string;
+  limit: number;
+  next: string;
+  offset: number;
+  previous: string;
+  total: number;
+  items: AlbumContainer[];
 }
 
 interface AlbumContainer {
-    added_at: string,
-    album: Album
+  added_at: string;
+  album: Album;
 }
 
 interface Album {
-    "album_type": string,
-    "total_tracks": number,
-    "available_markets": string[],
-    "external_urls": {
-        "spotify": string
-    },
-    "href": string,
-    "id": string,
-    "images": Images[],
-    "name": string,
-    "release_date": string,
-    "release_date_precision": string,
-    "restrictions": {
-        "reason": string
-    },
-    "type": string,
-    "uri": string,
-    "artists": Artist[],
-    "tracks": {
-        "href": string,
-        "limit": number,
-        "next": string,
-        "offset": number,
-        "previous": string,
-        "total": number,
-        "items": Track[]
-    },
-    "copyrights": Copyright[],
-    "external_ids": {
-        "isrc": string,
-        "ean": string,
-        "upc": string
-    },
-    "genres": string[],
-    "label": string,
-    "popularity": number
+  album_type: string;
+  total_tracks: number;
+  available_markets: string[];
+  external_urls: {
+    spotify: string;
+  };
+  href: string;
+  id: string;
+  images: Images[];
+  name: string;
+  release_date: string;
+  release_date_precision: string;
+  restrictions: {
+    reason: string;
+  };
+  type: string;
+  uri: string;
+  artists: Artist[];
+  tracks: {
+    href: string;
+    limit: number;
+    next: string;
+    offset: number;
+    previous: string;
+    total: number;
+    items: Track[];
+  };
+  copyrights: Copyright[];
+  external_ids: {
+    isrc: string;
+    ean: string;
+    upc: string;
+  };
+  genres: string[];
+  label: string;
+  popularity: number;
 }
 
 interface Images {
-    "url": string,
-    "height": number,
-    "width": number
+  url: string;
+  height: number;
+  width: number;
 }
 
 interface Artist {
-    "external_urls": {
-        "spotify": string
-    },
-    "href": string,
-    "id": string,
-    "name": string,
-    "type": artist,
-    "uri": string
+  external_urls: {
+    spotify: string;
+  };
+  href: string;
+  id: string;
+  name: string;
+  type: artist;
+  uri: string;
 }
 
 interface Track {
-    "artists": Artist[],
-    "available_markets": string[],
-    "disc_number": number,
-    "duration_ms": number,
-    "explicit": boolean,
-    "external_urls": {
-        "spotify": string
-    },
-    "href": string,
-    "id": string,
-    "is_playable": boolean,
-    "linked_from": {
-        "external_urls": {
-            "spotify": string
-        },
-        "href": string,
-        "id": string,
-        "type": string,
-        "uri": string
-    },
-    "restrictions": {
-        "reason": string
-    },
-    "name": string,
-    "preview_url": string,
-    "track_number": number,
-    "type": string,
-    "uri": string,
-    "is_local": boolean
+  artists: Artist[];
+  available_markets: string[];
+  disc_number: number;
+  duration_ms: number;
+  explicit: boolean;
+  external_urls: {
+    spotify: string;
+  };
+  href: string;
+  id: string;
+  is_playable: boolean;
+  linked_from: {
+    external_urls: {
+      spotify: string;
+    };
+    href: string;
+    id: string;
+    type: string;
+    uri: string;
+  };
+  restrictions: {
+    reason: string;
+  };
+  name: string;
+  preview_url: string;
+  track_number: number;
+  type: string;
+  uri: string;
+  is_local: boolean;
 }
 
 interface Copyright {
-    "text": string,
-    "type": string
+  text: string;
+  type: string;
 }
