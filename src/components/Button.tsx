1bf74eaacae5073ad359d7e5898596e50a747e88
Moritz Schüßler <mail@moritzschuessler.de>
2023-12-02 21:19:26 +0100

    Fixed that whole but isn't clickable

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,27 +1,33 @@
 import { ReactNode } from 'react';
 
 interface Props {
-  style?: 'solid';
+  style?: 'solid' | 'neutral-800';
   width?: 'full';
   children: ReactNode;
 }
 
 const Button = ({ style, width, children }: Props) => {
-  const buttonStyle = `
-    ${
-      style === 'solid'
-        ? 'bg-light text-dark hover:bg-neutral-300'
-        : 'hover:bg-neutral-800'
-    }`;
+  const buttonStyle = () => {
+    if (style === 'solid') {
+      return 'bg-light text-dark hover:bg-neutral-300';
+    }
+    if (style === 'neutral-800') {
+      return 'bg-neutral-800';
+    }
+    return 'hover:bg-neutral-800';
+  };
 
-  const buttonWidth = `
-    ${width === 'full' ? 'w-full' : ''} 
-  `;
+  const buttonWidth = () => {
+    if (width === 'full') {
+      return 'children:w-full';
+    }
+    return 'children:px-[1rem] children:py-[.5rem]';
+  };
 
   return (
     <div
-      className={`${buttonStyle} ${buttonWidth} 
-        flex items-center justify-center rounded-md transition duration-75 children:flex children:px-[1rem] children:py-[.5rem]`}
+      className={`${buttonStyle()} ${buttonWidth()} 
+        flex items-center justify-center rounded-md transition duration-75 children:flex children:items-center children:justify-center children:py-[.5rem]`}
     >
       {children}
     </div>
