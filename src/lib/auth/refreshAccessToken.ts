e78a7d295aaeffd0765cb5166af807db41997c58
Moritz Schüßler <mail@moritzschuessler.de>
2023-12-04 21:48:57 +0100

    refactor: Logic for refreshing access_token

--- src/lib/auth/refreshAccessToken.ts
+++ src/lib/auth/refreshAccessToken.ts
@@ -1,34 +1,27 @@
-import { NextAuthOptions } from 'next-auth';
 import { JWT } from 'next-auth/jwt';
 import { URLSearchParams } from 'url';
-import SpotifyProvider from 'next-auth/providers/spotify';
 
 const refreshAccessToken = async (token: JWT) => {
-  try {
-    const response = await fetch('https://accounts.spotify.com/api/token', {
-      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-      body: new URLSearchParams({
-        client_id: process.env.SPOTIFY_ID!,
-        client_secret: process.env.SPOTIFY_SECRET!,
-        grant_type: 'refresh_token',
-        refresh_token: token.refresh_token,
-      }),
-      method: 'POST',
-    });
+  const response = await fetch('https://accounts.spotify.com/api/token', {
+    headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+    body: new URLSearchParams({
+      client_id: process.env.SPOTIFY_ID!,
+      client_secret: process.env.SPOTIFY_SECRET!,
+      grant_type: 'refresh_token',
+      refresh_token: token.refresh_token,
+    }),
+    method: 'POST',
+  });
 
-    const refreshedTokens = await response.json();
+  const refreshedTokens = await response.json();
 
-    if (!response.ok) {
-      throw Error(refreshedTokens);
-    }
-
-    token.access_token = refreshedTokens.access_token;
-    token.refresh_token = refreshedTokens.refresh_token ?? token.refresh_token;
-    token.expires_at = Date.now() + refreshedTokens.expires_in * 1000;
-  } catch (error) {
-    console.log(error);
+  if (!response.ok) {
     token.error = 'RefreshAccessTokenError';
   }
+
+  token.access_token = refreshedTokens.access_token;
+  token.refresh_token = refreshedTokens.refresh_token ?? token.refresh_token;
+  token.expires_at = Date.now() + refreshedTokens.expires_in * 1000;
 
   return token;
 };
