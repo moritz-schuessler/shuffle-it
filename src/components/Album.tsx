3d7f64be6a32dbe7255ff544ec5058e8bb78ffcc
Moritz Schüßler <mail@moritzschuessler.de>
2023-12-26 23:40:59 +0100

    refactor: add a button for playing an album

--- src/components/Album.tsx
+++ src/components/Album.tsx
@@ -13,27 +13,32 @@ const Album = ({ album, access_token }: Props) => {
   const { name, uri, artists, images } = album;
 
   return (
-    <button
-      onClick={() => mutation.mutate({ access_token, uri })}
-      className='flex grow flex-col gap-[1rem] rounded-md bg-neutral-900 p-[1rem] transition hover:bg-neutral-800'
-    >
-      <div className='flex grow'>
-        <Image
-          src={images[0].url}
-          alt={`Album Cover of ${name}`}
-          width={300}
-          height={300}
-          unoptimized
-          className='aspect-square grow rounded'
-        />
-      </div>
-      <div className='flex flex-col  children:truncate'>
-        <a>{name}</a>
-        <div className='text-neutral-400'>
-          {artists.map((artist) => artist.name).join(', ')}
+    <div className='flex grow flex-col overflow-clip rounded-md'>
+      <div className='flex grow flex-col gap-[1rem] bg-neutral-900 p-[1rem] transition hover:bg-neutral-800'>
+        <div className='flex'>
+          <Image
+            src={images[0].url}
+            alt={`Album Cover of ${name}`}
+            width={300}
+            height={300}
+            unoptimized
+            className='aspect-square grow rounded'
+          />
+        </div>
+        <div className='flex flex-col children:truncate'>
+          <a>{name}</a>
+          <div className='text-neutral-400'>
+            {artists.map((artist) => artist.name).join(', ')}
+          </div>
         </div>
       </div>
-    </button>
+      <button
+        onClick={() => mutation.mutate({ access_token, uri })}
+        className='bg-neutral-900 p-[1rem] hover:bg-neutral-800'
+      >
+        Play
+      </button>
+    </div>
   );
 };
 
