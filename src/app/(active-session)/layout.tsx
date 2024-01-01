73fd41ce44dbadbf26d3a00dc4f89704e7bff32f
Moritz Schüßler <mail@moritzschuessler.de>
2024-01-01 16:57:55 +0100

    feat: add a footer

--- src/app/(active-session)/layout.tsx
+++ src/app/(active-session)/layout.tsx
@@ -2,6 +2,11 @@
 
 import { ReactNode, useEffect } from 'react';
 import { signIn, useSession } from 'next-auth/react';
+import Link from 'next/link';
+
+import Button from '@/components/Button';
+import GithubIcon from '@/assets/icons/GithubIcon';
+import SpotifyIcon from '@/assets/icons/SpotifyIcon';
 
 interface Props {
   children: ReactNode;
@@ -19,10 +24,43 @@ const Layout = ({ children, albums }: Props) => {
   }, [session]);
 
   return (
-    <main className='flex overflow-hidden children:w-full'>
-      {children}
+    <div className='flex h-full overflow-hidden children:w-full'>
+      <div className='flex h-full flex-col justify-between'>
+        <main className='h-full'>{children}</main>
+        <Footer />
+      </div>
       {albums}
-    </main>
+    </div>
+  );
+};
+
+const Footer = () => {
+  return (
+    <footer className='flex items-center justify-center gap-4 p-8'>
+      <Button variant='ghost' asChild>
+        <Link href={'/privacy'}>Privacy</Link>
+      </Button>
+      <Button variant='ghost' asChild>
+        <Link
+          href={'https://github.com/moritz-schuessler/shuffle-it'}
+          target='_blank'
+          className='flex gap-2'
+        >
+          <GithubIcon color='white' />
+          <div>Project</div>
+        </Link>
+      </Button>
+      <Button variant='ghost' asChild>
+        <Link
+          href={'https://open.spotify.com/'}
+          target='_blank'
+          className='flex gap-2'
+        >
+          <SpotifyIcon color='white' />
+          <div>Spotify</div>
+        </Link>
+      </Button>
+    </footer>
   );
 };
 
