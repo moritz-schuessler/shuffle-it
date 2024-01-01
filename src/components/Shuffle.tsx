b8f5aa25027b5ffb90d1f404dfd0426697bf855a
Moritz Schüßler <mail@moritzschuessler.de>
2024-01-01 21:07:35 +0100

    feat(state - totalSavedAlbums): use totalSavedAlbums in Shuffle

--- src/components/Shuffle.tsx
+++ src/components/Shuffle.tsx
@@ -1,32 +1,24 @@
 import { useSession } from 'next-auth/react';
-import { redirect } from 'next/navigation';
+import { useAtomValue } from 'jotai';
 
-import usePlayback from '@/hooks/usePlayback';
-import { getAlbum } from '@/lib/spotifyApi';
 import Button from '@/components/Button';
+import usePlayback from '@/hooks/usePlayback';
+import totalSavedAlbumsAtom from '@/lib/atoms/totalSavedAlbumsAtom';
+import { getAlbum } from '@/lib/spotifyApi';
 
-interface Props {
-  resource: 'album';
-  amountOfResource: number;
-}
-
-const Shuffle = ({ resource, amountOfResource }: Props) => {
+const Shuffle = () => {
   const { data: session } = useSession();
-  if (!session) {
-    redirect('/signin');
-  }
+  const totalSavedAlbums = useAtomValue(totalSavedAlbumsAtom);
 
   const mutation = usePlayback();
 
   const handleClick = async () => {
-    let uri = '';
-    const offset = Math.floor(Math.random() * amountOfResource) - 1;
-    if (resource === 'album') {
-      const response = await getAlbum(session.access_token!, offset);
-      const album = response.items[0].album;
-      uri = album.uri;
-    }
-    mutation.mutate({ access_token: session.access_token!, uri });
+    const offset = Math.floor(Math.random() * totalSavedAlbums) - 1;
+
+    const response = await getAlbum(session!.access_token!, offset);
+    const album = response.items[0].album;
+
+    mutation.mutate({ access_token: session!.access_token!, uri: album.uri });
   };
 
   return (
