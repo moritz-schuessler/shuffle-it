39a64584f3251e87ac92431540db14c88c78fd3f
Moritz Schüßler <mail@moritzschuessler.de>
2024-01-03 02:28:23 +0100

    feat: add a short Q&A section on the main page

--- src/app/(active-session)/page.tsx
+++ src/app/(active-session)/page.tsx
@@ -1,9 +1,46 @@
+'use client';
+
 import Shuffle from '@/components/Shuffle';
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from '@/components/Accordion';
 
 const Home = () => {
   return (
-    <main className='flex h-full w-full flex-col items-stretch p-8'>
+    <main className='flex h-full w-full flex-col items-stretch justify-between p-8'>
       <Shuffle />
+      <Accordion type='single' collapsible>
+        <AccordionItem value='item-1'>
+          <AccordionTrigger>
+            What issue does this project aim to solve?
+          </AccordionTrigger>
+          <AccordionContent>
+            Spotify does not offer a option to Shuffle your Saved Albums. This
+            web app tries to solve this problem.
+          </AccordionContent>
+        </AccordionItem>
+        <AccordionItem value='item-2'>
+          <AccordionTrigger>
+            How does this app solve the problem?
+          </AccordionTrigger>
+          <AccordionContent>
+            By integrating with the Spotify API, this project solves the problem
+            by retrieving saved albums and controlling the playback.
+          </AccordionContent>
+        </AccordionItem>
+        <AccordionItem value='item-3'>
+          <AccordionTrigger>
+            What are the prerequisites for using the app?
+          </AccordionTrigger>
+          <AccordionContent>
+            To use the app, you need a Spotify Account and an active instance of
+            the Spotify app.
+          </AccordionContent>
+        </AccordionItem>
+      </Accordion>
     </main>
   );
 };
