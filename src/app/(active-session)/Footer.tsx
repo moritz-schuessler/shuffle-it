0cfe3262d12c6d9905c6c462b175cc0da83ced5b
Moritz Schüßler <mail@moritzschuessler.de>
2024-01-05 00:09:39 +0100

    refactor: move Footer into a separate file

---
+++ src/app/(active-session)/Footer.tsx
@@ -0,0 +1,37 @@
+import Link from 'next/link';
+
+import Button from '@/components/Button';
+import GithubIcon from '@/assets/icons/GithubIcon';
+import SpotifyIcon from '@/assets/icons/SpotifyIcon';
+
+const Footer = () => {
+  return (
+    <footer className='flex items-center justify-center gap-4'>
+      <Button variant='ghost' asChild>
+        <Link href={'/privacy'}>Privacy</Link>
+      </Button>
+      <Button variant='ghost' asChild>
+        <Link
+          href={'https://github.com/moritz-schuessler/shuffle-it'}
+          target='_blank'
+          className='flex gap-2'
+        >
+          <GithubIcon color='white' />
+          <div>Project</div>
+        </Link>
+      </Button>
+      <Button variant='ghost' asChild>
+        <Link
+          href={'https://open.spotify.com/'}
+          target='_blank'
+          className='flex gap-2'
+        >
+          <SpotifyIcon color='white' />
+          <div>Spotify</div>
+        </Link>
+      </Button>
+    </footer>
+  );
+};
+
+export default Footer;
--- src/app/(active-session)/layout.tsx
+++ src/app/(active-session)/layout.tsx
@@ -1,12 +1,9 @@
 'use client';
 
 import { ReactNode, useEffect } from 'react';
-import Link from 'next/link';
 import { signIn, useSession } from 'next-auth/react';
 
-import Button from '@/components/Button';
-import GithubIcon from '@/assets/icons/GithubIcon';
-import SpotifyIcon from '@/assets/icons/SpotifyIcon';
+import Footer from '@/app/(active-session)/Footer';
 
 interface Props {
   children: ReactNode;
@@ -33,36 +30,6 @@ const Layout = ({ children, albums, signin }: Props) => {
       {status === 'authenticated' && albums}
       {status === 'unauthenticated' && signin}
     </div>
-  );
-};
-
-const Footer = () => {
-  return (
-    <footer className='flex items-center justify-center gap-4'>
-      <Button variant='ghost' asChild>
-        <Link href={'/privacy'}>Privacy</Link>
-      </Button>
-      <Button variant='ghost' asChild>
-        <Link
-          href={'https://github.com/moritz-schuessler/shuffle-it'}
-          target='_blank'
-          className='flex gap-2'
-        >
-          <GithubIcon color='white' />
-          <div>Project</div>
-        </Link>
-      </Button>
-      <Button variant='ghost' asChild>
-        <Link
-          href={'https://open.spotify.com/'}
-          target='_blank'
-          className='flex gap-2'
-        >
-          <SpotifyIcon color='white' />
-          <div>Spotify</div>
-        </Link>
-      </Button>
-    </footer>
   );
 };
 
