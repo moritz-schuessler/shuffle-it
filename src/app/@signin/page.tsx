cde7f9ce64675bac6209c512b624cc758400b0db
Moritz Schüßler <mail@moritzschuessler.de>
2024-01-05 01:18:21 +0100

    refactor: change content and style of sign in page

--- src/app/@signin/page.tsx
+++ src/app/@signin/page.tsx
@@ -6,19 +6,21 @@ const SignIn = async () => {
   const providers = await getProviders();
 
   return (
-    <main className='flex h-full items-center justify-center overflow-scroll'>
-      <div className='flex w-1/2 max-w-lg flex-col gap-6'>
-        <div className='flex flex-col items-center justify-center gap-2'>
-          <h2 className='text-xl'>Sign in with Spotify</h2>
-          <p className='text-neutral-400'>
-            To use this app it&#39;s required to be signed in with Spotify
-          </p>
-        </div>
-        {Object.values(providers!).map((provider) => {
-          return <SignInButton key={provider.id} provider={provider} />;
-        })}
+    <div className='flex flex-col items-center justify-center gap-6 overflow-scroll bg-neutral-900'>
+      <div className='flex flex-col items-center justify-center gap-2'>
+        <h2 className='text-xl'>Sign in with Spotify</h2>
+        <p className='text-neutral-400'>
+          To use this app, sign in with your Spotify account{' '}
+        </p>
       </div>
-    </main>
+      {Object.values(providers!).map((provider) => {
+        return (
+          <div key={provider.id} className='flex w-1/2 max-w-lg flex-col'>
+            <SignInButton provider={provider} />
+          </div>
+        );
+      })}
+    </div>
   );
 };
 
