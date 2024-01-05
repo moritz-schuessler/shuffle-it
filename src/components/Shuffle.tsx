f15ec0a54ae755e5cc3d238ca5f7dfa576730cd3
Moritz Schüßler <mail@moritzschuessler.de>
2024-01-05 17:42:58 +0100

    refactor: move logic from spotifyApi into the Shuffle-component

--- src/components/Shuffle.tsx
+++ src/components/Shuffle.tsx
@@ -6,7 +6,6 @@ import { useAtomValue } from 'jotai';
 import Button from '@/components/ui/Button';
 import usePlayback from '@/hooks/usePlayback';
 import totalSavedAlbumsAtom from '@/lib/atoms/totalSavedAlbumsAtom';
-import { getAlbum } from '@/lib/spotifyApi';
 
 const Shuffle = () => {
   const { data: session } = useSession();
@@ -17,8 +16,18 @@ const Shuffle = () => {
   const handleClick = async () => {
     const offset = Math.floor(Math.random() * totalSavedAlbums) - 1;
 
-    const response = await getAlbum(session!.access_token!, offset);
-    const album = response.items[0].album;
+    const response = await fetch(
+      `https://api.spotify.com/v1/me/albums?offset=${offset}&limit=1&locale=*`,
+      {
+        method: 'GET',
+        headers: {
+          Authorization: `Bearer ${session!.access_token!}`,
+        },
+      },
+    );
+
+    const data: Albums = await response.json();
+    const album = data.items[0].album;
 
     mutation.mutate({ access_token: session!.access_token!, uri: album.uri });
   };
