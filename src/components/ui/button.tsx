8b3e7ba0b660bcf1213b5e6f9b8dba430c6fc2b6
Moritz Schüßler <mail@moritzschuessler.de>
2024-02-01 22:05:43 +0100

    feat(styling): add disabled state to buttons

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -4,28 +4,31 @@ import { ButtonHTMLAttributes } from 'react';
 import { Slot } from '@radix-ui/react-slot';
 import { cva, VariantProps } from 'class-variance-authority';
 
-const buttonVariants = cva(['transition duration-75 rounded-md'], {
-  variants: {
-    variant: {
-      default: 'bg-light text-dark hover:bg-neutral-300',
-      secondary: 'bg-neutral-900 hover:bg-neutral-800',
-      ghost: 'hover:bg-neutral-800',
+const buttonVariants = cva(
+  ['transition duration-75 rounded-md disabled:opacity-60'],
+  {
+    variants: {
+      variant: {
+        default: 'bg-light text-dark enabled:hover:bg-neutral-300',
+        secondary: 'bg-neutral-900 enabled:hover:bg-neutral-800',
+        ghost: 'hover:bg-neutral-800',
+      },
+      rounded: {
+        none: 'rounded-none',
+        md: 'rounded-md',
+      },
+      padding: {
+        default: 'px-4 py-2',
+        none: '',
+      },
     },
-    rounded: {
-      none: 'rounded-none',
-      md: 'rounded-md',
-    },
-    padding: {
-      default: 'px-4 py-2',
-      none: '',
+    defaultVariants: {
+      variant: 'default',
+      rounded: 'md',
+      padding: 'default',
     },
   },
-  defaultVariants: {
-    variant: 'default',
-    rounded: 'md',
-    padding: 'default',
-  },
-});
+);
 
 interface Props
   extends ButtonHTMLAttributes<HTMLButtonElement>,
