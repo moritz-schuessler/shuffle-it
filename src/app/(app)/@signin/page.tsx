1d6a2befbed4d08705354a66c3b4d755e68fca49
moritz-schuessler <mail@moritzschuessler.de>
2024-09-23 22:05:33 +0200

    fix: adds aside tag back to signin path

--- src/app/(app)/@signin/page.tsx
+++ src/app/(app)/@signin/page.tsx
@@ -3,18 +3,20 @@ import SpotifyIcon from '@/assets/icons/spotify-icon';
 
 const SignIn = async () => {
   return (
-    <div className='mobile:bg-none flex h-full flex-col items-center justify-center gap-6 bg-gray-100'>
-      <div className='flex flex-col items-center justify-center gap-2'>
-        <h2 className='text-xl'>Sign in with Spotify</h2>
-        <p className='text-center text-gray-300'>
-          To use this app, sign in with your Spotify account{' '}
-        </p>
+    <aside className='h-full w-full overflow-scroll px-[var(--window-padding-x)] py-[var(--window-padding-y)]'>
+      <div className='flex h-full flex-col items-center justify-center gap-6 bg-gray-100 mobile:bg-none'>
+        <div className='flex flex-col items-center justify-center gap-2'>
+          <h2 className='text-xl'>Sign in with Spotify</h2>
+          <p className='text-center text-gray-300'>
+            To use this app, sign in with your Spotify account{' '}
+          </p>
+        </div>
+        <SignInButton className='flex w-1/2 max-w-lg items-center justify-center gap-2'>
+          <SpotifyIcon color='black' />
+          Spotify
+        </SignInButton>
       </div>
-      <SignInButton className='flex w-1/2 max-w-lg items-center justify-center gap-2'>
-        <SpotifyIcon color='black' />
-        Spotify
-      </SignInButton>
-    </div>
+    </aside>
   );
 };
 
