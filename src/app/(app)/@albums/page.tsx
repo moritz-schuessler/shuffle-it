23cc88ea6749caf96269b7ac349c8b01d530a749
moritz-schuessler <mail@moritzschuessler.de>
2024-09-24 01:27:41 +0200

    feat: adds buttons to toggle library on main section

--- src/app/(app)/@albums/page.tsx
+++ src/app/(app)/@albums/page.tsx
@@ -1,10 +1,28 @@
 import AlbumLibrary from '@/app/(app)/@albums/album-library';
+import Button from '@/components/ui/button';
+import Link from 'next/link';
+import LibraryIcon from '@/assets/icons/library-icon';
 
 const Albums = ({ searchParams }: { searchParams: { library: string } }) => {
-  const library = searchParams.library;
+  const library = searchParams.library !== undefined;
 
-  if (library === undefined) {
-    return;
+  if (library) {
+    return (
+      <aside className='h-full px-[var(--window-padding-x)] py-[var(--window-padding-y)]'>
+        <Button
+          variant='secondary'
+          className='flex h-full flex-col gap-4'
+          asChild
+        >
+          <Link href={`?${library ? '' : 'library'}`}>
+            <LibraryIcon />
+            <span className='rotate-180 [writing-mode:vertical-lr]'>
+              Show Library
+            </span>
+          </Link>
+        </Button>
+      </aside>
+    );
   }
 
   return (
