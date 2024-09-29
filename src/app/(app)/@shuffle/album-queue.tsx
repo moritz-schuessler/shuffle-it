4cfc608f3e8b1803e4e7b47038d0d8538f8dc945
moritz-schuessler <mail@moritzschuessler.de>
2024-09-29 03:00:37 +0200

    feat: adds a button to delete items on the queue

--- src/app/(app)/@shuffle/album-queue.tsx
+++ src/app/(app)/@shuffle/album-queue.tsx
@@ -1,9 +1,15 @@
-import { useQueryClient } from '@tanstack/react-query';
+import { useQuery, useQueryClient } from '@tanstack/react-query';
 import Image from 'next/image';
+import Button from '@/components/ui/button';
+import { Cross1Icon } from '@radix-ui/react-icons';
 
 const AlbumQueue = () => {
   const queryClient = useQueryClient();
-  const data = queryClient.getQueryData(['queue']) as Album[];
+
+  const { data } = useQuery<Album[]>({
+    queryKey: ['queue'],
+    initialData: [],
+  });
 
   return (
     <>
@@ -15,23 +21,38 @@ const AlbumQueue = () => {
           return (
             <div
               key={index + album.id}
-              className='flex items-center gap-2 rounded-md bg-gray-200 p-2'
+              className='flex items-center justify-between rounded-md bg-gray-200 p-2'
             >
-              <div className='flex aspect-square w-12 grow-0'>
-                <Image
-                  src={album.images[0].url}
-                  alt={`Album Cover of ${album.name}`}
-                  width={300}
-                  height={300}
-                  unoptimized
-                  className='aspect-square grow rounded'
-                />
+              <div className='flex gap-2'>
+                <div className='flex aspect-square w-12 grow-0'>
+                  <Image
+                    src={album.images[0].url}
+                    alt={`Album Cover of ${album.name}`}
+                    width={300}
+                    height={300}
+                    unoptimized
+                    className='aspect-square grow rounded'
+                  />
+                </div>
+                <div className='flex flex-col'>
+                  <span className='truncate'>{album.name}</span>
+                  <span className='truncate text-gray-300'>
+                    {album.artists.map((artist) => artist.name).join(', ')}
+                  </span>
+                </div>
               </div>
-              <div className='flex flex-col'>
-                <span className='truncate'>{album.name}</span>
-                <span className='truncate text-gray-300'>
-                  {album.artists.map((artist) => artist.name).join(', ')}
-                </span>
+              <div>
+                <Button
+                  variant='ghost'
+                  onClick={() => {
+                    queryClient.setQueryData(['queue'], (oldData: Album[]) => [
+                      ...oldData.filter((value, i) => i !== index),
+                    ]);
+                    queryClient.invalidateQueries({ queryKey: ['queue'] });
+                  }}
+                >
+                  <Cross1Icon aria-label='Remove Album from Queue' />
+                </Button>
               </div>
             </div>
           );
