96ec77ae04d57ca40406b23f59c39ad5e06e0085
Moritz Schüßler <mail@moritzschuessler.de>
2024-10-28 22:15:17 +0100

    fix: double footer on privacy page

--- src/app/(legal)/layout.tsx
+++ src/app/(legal)/layout.tsx
@@ -7,11 +7,10 @@ interface Props {
 
 const LegalLayout = ({ children }: Props) => {
   return (
-    <div className='mobile:gap-4 flex flex-col items-center gap-8 overflow-hidden *:px-[var(--window-padding-x)] first:*:pt-[var(--window-padding-y)] last:*:pb-[var(--window-padding-y)]'>
+    <div className='flex flex-col items-center gap-8 overflow-hidden *:px-[var(--window-padding-x)] first:*:pt-[var(--window-padding-y)] last:*:pb-[var(--window-padding-y)] mobile:gap-4'>
       <main className='flex w-full justify-center overflow-scroll hyphens-auto text-pretty break-words'>
         {children}
       </main>
-      <Footer />
     </div>
   );
 };
