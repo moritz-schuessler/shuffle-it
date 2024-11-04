08803dd8245033f28c0babfe51c571400277ef09
Moritz Schüßler <mail@moritzschuessler.de>
2024-11-05 00:32:17 +0100

    style: change border radius

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -4,22 +4,19 @@ import { ButtonHTMLAttributes } from 'react';
 import { Slot } from '@radix-ui/react-slot';
 import { cva, VariantProps } from 'class-variance-authority';
 
-const buttonVariants = cva(
-  ['transition duration-75 rounded-md px-2 py-2 rounded-md'],
-  {
-    variants: {
-      variant: {
-        default: 'bg-button hover:bg-hover-100',
-        secondary: 'bg-white text-black hover:bg-hover-200 bg-button-secondary',
-        ghost: 'bg-button-ghost hover:bg-hover-100',
-        dashed: 'border-dashed border-2 border-border hover:border-hover-100',
-      },
-    },
-    defaultVariants: {
-      variant: 'default',
+const buttonVariants = cva(['transition duration-75 rounded-md px-2 py-2'], {
+  variants: {
+    variant: {
+      default: 'bg-button hover:bg-hover-100',
+      secondary: 'bg-white text-black hover:bg-hover-200 bg-button-secondary',
+      ghost: 'bg-button-ghost hover:bg-hover-100',
+      dashed: 'border-dashed border-2 border-border hover:border-hover-100',
     },
   },
-);
+  defaultVariants: {
+    variant: 'default',
+  },
+});
 
 interface Props
   extends ButtonHTMLAttributes<HTMLButtonElement>,
